e1edde2c5093b3af26cfc841de5f66c2d5c95e13
Serdar Olmez <serdarolmez0@gmail.com>
2023-08-24 19:04:34 +0200

    formulaire finalisée

--- GameOn-website-FR-master/starterOnly/modal.js
+++ GameOn-website-FR-master/starterOnly/modal.js
@@ -1,3 +1,22 @@
+
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeCross = document.querySelectorAll('.close')
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthday = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const cityLocation = document.querySelectorAll(".radio input");
+const checkbox1 = document.getElementById("checkbox1");
+const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/;
+const emailReg = new RegExp(
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/i
+    );
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,12 +25,6 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closeCross = document.querySelectorAll('.close')
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -33,84 +46,89 @@ function closeModal() {
 
 // Validation du formulaire avec message sous chaques champs en cas d'erreurs
 
-  
+   // Validation du formulaire au moment de la soumission
+   const buttonForm = document.querySelector(".btn-submit")
 
-  function validateForm() {
-      const firstName = document.getElementById("first")
-      const lastName = document.getElementById("last")
-      const email = document.getElementById("email")
-      const birthday = document.getElementById("birthdate")
-      const quantity = document.getElementById("quantity")
-      const cityLocation = document.querySelectorAll('.radio input')
-      const checkbox1 = document.getElementById("checkbox1")
-      const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/
-      const emailReg = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/i);
-
+   buttonForm.addEventListener("click", function (e) {
+    e.preventDefault();
+    formulaireValidator()  
+   });
 
     // Validation du prénom
-
-    function firstNameValidator() {
-    
-      if(firstName.trim()==="" || !firstName.value.match(regex) || firstName.length < 2){
-        firstName.style.border = '1px solid #c80014'
-        document.getElementById("firstError").textContent = "non rempli"
-        return false
-      }else{
-        document.getElementById("firstError").textContent = ""
-        return true
-      }
+  function firstNameValidator() {
+    if (
+      firstName.value.trim() === "" ||
+      !firstName.value.match(regex) ||
+      firstName.value.length < 2
+    ) {
+      document.getElementById("firstError").textContent =
+        "Veuillez entrer un prénom valide.";
+      return false;
+    } else {
+      document.getElementById("firstError").textContent = "";
+      return true;
     }
-
-    // Validation du nom de famille 
-
-    function lastNameValidator() {
-
-      if(lastName.trim()==="" || !lastName.value.match(regex) || lastName.length < 2){
-        firstName.style.border = '1px solid #c80014'
-        document.getElementById("lastError").textContent = "non rempli"
-        return false
-      }else{
-        document.getElementById("lastError").textContent = ""
-        return true
-      }
-    }
-
-    // Validation de l'email
-
-    function emailValidator() {
-      if(email.trim()==="" || !email.value.match(emailReg) ) {
-        firstName.style.border = '1px solid #c80014'
-        document.getElementById("lastError").textContent = "non rempli"
-      }else {
-        document.getElementById("lastError").textContent = ""
-        return true
-      }
-    }
-
-
-    // Validation de la date de naissance
-
-    function birthValidator() {
-      if(birthday.trim ==="" || birthday.length != 10) {
-        firstName.style.border = '1px solid #c80014'
-        document.getElementById("lastError").textContent = "non rempli"
-      }else {
-        document.getElementById("lastError").textContent = ""
-        return true
-      }
-    }
-
-    
-    // validation de la quantité de participation
-
-    function quantityValidator() {
-      if(quantity.trim ==="" || quantity.length < 1) {
-        firstName.style.border = '1px solid #c80014'
-        document.getElementById("lastError").textContent = "non rempli"
-      }else {
-        document.getElementById("lastError").textContent = ""
-        return true
-      }
-    }
-
   }
+
+  // Validation du nom de famille
+  function lastNameValidator() {
+    if (
+      lastName.value.trim() === "" ||
+      !lastName.value.match(regex) ||
+      lastName.value.length < 2
+    ) {
+      document.getElementById("lastError").textContent =
+        "Veuillez entrer un nom de famille valide.";
+      return false;
+    } else {
+      document.getElementById("lastError").textContent = "";
+      return true;
+    }
+  }
+
+  // Validation de l'email
+  function emailValidator() {
+    if (email.value.trim() === "" || !email.value.match(emailReg)) {
+      document.getElementById("emailError").textContent =
+        "Veuillez entrer une adresse e-mail valide.";
+      return false;
+    } else {
+      document.getElementById("emailError").textContent = "";
+      return true;
+    }
+  }
+
+  // Validation de la date de naissance
+  function birthValidator() {
+    if (birthday.value.trim() === "" || birthday.value.length !== 10) {
+      document.getElementById("birthdateError").textContent =
+        "Veuillez entrer une date de naissance valide (format YYYY-MM-DD).";
+      return false;
+    } else {
+      document.getElementById("birthdateError").textContent = "";
+      return true;
+    }
+  }
+
+  // Validation de la quantité de participation
+  function quantityValidator() {
+    if (quantity.value.trim() === "" || quantity.value < 1) {
+      document.getElementById("quantityError").textContent =
+        "Veuillez entrer une quantité valide.";
+      return false;
+    } else {
+      document.getElementById("quantityError").textContent = "";
+      return true;
+    }
+  }
+
+  function formulaireValidator() {
+
+    firstNameValidator() 
+    lastNameValidator() 
+    emailValidator() 
+    birthValidator() 
+    quantityValidator()
+  
+}
+
